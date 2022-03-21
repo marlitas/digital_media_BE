761348f6dfbe71bb9d4cb02a25e093800e401937
Marla Schulz <marlaa.schulz@gmail.com>
2022-03-21 11:25:48 -0600

    Set origin to all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://digital-media-arts.herokuapp.com/', 'http://localhost:3000']
+    origins '*'
 
-    resource '/api/v1/*',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 
